b70b409f80c237f938ce82cf1292b9c49896d2d9
justIsal <tsalmanisal@gmail.com>
2025-01-09 17:26:17 +0700

    update ppdb

--- src/app/(no-auth)/ppdb/_components/notFound.jsx
+++ src/app/(no-auth)/ppdb/_components/notFound.jsx
@@ -5,7 +5,7 @@ function PPDBBelumDibuka({ data }) {
     <div className="bg-gradient-to-b from-teal-200 to-teal-400 p-4 md:p-0 text-black h-[40vh]">
       <div className="h-full w-full flex items-center justify-center">
         <h2 className="text-2xl font-bold text-gray-800">
-          <i>{data?.message}</i>
+          <i>PPDB BELUM DI BUKA</i>
         </h2>
       </div>
     </div>
